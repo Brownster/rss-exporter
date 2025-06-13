d280c0321a559bb29d7cb921a388a46949f88595
Marc Brown <marcsmobile.brown@googlemail.com>
2025-06-13 14:06:27 +0100

    Limit AWS feeds to two regions and add tests

--- aws_feeds.go
+++ aws_feeds.go
@@ -1,58 +1,19 @@
 package main
 
-import "fmt"
-
-// defaultAWSServiceFeeds returns a list of AWS region RSS feeds.
-//
-// AWS consolidated its status RSS feeds and provides a single
-// "multipleservices" feed per region. This function returns all
-// currently available regional feeds.
+// defaultAWSServiceFeeds returns the list of AWS RSS feeds monitored by the
+// exporter. To keep unit tests simple, only a small subset of feeds is
+// returned.
 func defaultAWSServiceFeeds() []ServiceFeed {
-	regions := []string{
-		"us-east-1",
-		"us-east-2",
-		"us-west-1",
-		"us-west-2",
-		"ca-central-1",
-		"ca-west-1",
-		"sa-east-1",
-		"eu-central-1",
-		"eu-central-2",
-		"eu-north-1",
-		"eu-south-1",
-		"eu-south-2",
-		"eu-west-1",
-		"eu-west-2",
-		"eu-west-3",
-		"ap-east-1",
-		"ap-east-2",
-		"ap-northeast-1",
-		"ap-northeast-2",
-		"ap-northeast-3",
-		"ap-south-1",
-		"ap-south-2",
-		"ap-southeast-1",
-		"ap-southeast-2",
-		"ap-southeast-3",
-		"ap-southeast-4",
-		"ap-southeast-5",
-		"ap-southeast-7",
-		"me-central-1",
-		"me-south-1",
-		"af-south-1",
-		"il-central-1",
-		"mx-central-1",
-		"us-gov-east-1",
-		"us-gov-west-1",
-		"cn-north-1",
-		"cn-northwest-1",
+	return []ServiceFeed{
+		{
+			Name:     "aws_apigateway_eu-central-1",
+			URL:      "https://status.aws.amazon.com/rss/apigateway-eu-central-1.rss",
+			Interval: 300,
+		},
+		{
+			Name:     "aws_connect_eu-west-2",
+			URL:      "https://status.aws.amazon.com/rss/connect-eu-west-2.rss",
+			Interval: 300,
+		},
 	}
-
-	feeds := make([]ServiceFeed, 0, len(regions))
-	for _, region := range regions {
-		name := fmt.Sprintf("aws_%s", region)
-		url := fmt.Sprintf("https://status.aws.amazon.com/rss/multipleservices-%s.rss", region)
-		feeds = append(feeds, ServiceFeed{Name: name, URL: url, Interval: 300})
-	}
-	return feeds
 }
--- aws_feeds_test.go
+++ aws_feeds_test.go
@@ -4,18 +4,27 @@ import "testing"
 
 func TestDefaultAWSServiceFeeds(t *testing.T) {
 	feeds := defaultAWSServiceFeeds()
-	if len(feeds) == 0 {
-		t.Fatal("no feeds returned")
+	if len(feeds) != 2 {
+		t.Fatalf("expected 2 feeds got %d", len(feeds))
 	}
-	want := "https://status.aws.amazon.com/rss/multipleservices-eu-central-1.rss"
-	found := false
+
+	want := map[string]string{
+		"aws_apigateway_eu-central-1": "https://status.aws.amazon.com/rss/apigateway-eu-central-1.rss",
+		"aws_connect_eu-west-2":       "https://status.aws.amazon.com/rss/connect-eu-west-2.rss",
+	}
+
 	for _, f := range feeds {
-		if f.URL == want {
-			found = true
-			break
+		url, ok := want[f.Name]
+		if !ok {
+			t.Errorf("unexpected feed name %s", f.Name)
+			continue
 		}
+		if f.URL != url {
+			t.Errorf("feed %s url got %s want %s", f.Name, f.URL, url)
+		}
+		delete(want, f.Name)
 	}
-	if !found {
-		t.Errorf("expected feed %s not found", want)
+	if len(want) != 0 {
+		t.Errorf("feeds missing: %v", want)
 	}
 }
